2c7ab39e99ff464eedbe349e3455647084816a62
Sayuni <118969889+Satoru24@users.noreply.github.com>
2025-04-08 19:32:39 +0530

    first

--- .venv/main.py
+++ .venv/main.py
@@ -1,7 +1,12 @@
 import cv2
+import numpy as np
 
-img = cv2.imread('lena.png')
+thres = 0.6 #Threshold to detect objects
 
+#img = cv2.imread('lena.png')
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
 classNames = []
 classFile = 'coco.names'
@@ -17,13 +22,20 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean([127.5, 127.5, 127.5])  # For RGB images
 net.setInputSwapRB(True)
 
-classIds, confs, bbox = net.detect(img, confThreshold=0.5)
-print(classIds,bbox)
+while True:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+    print(classIds,bbox)
 
-for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
-    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(np.array(classIds).flatten(), np.array(confs).flatten(), bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+            cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),
+                        cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            cv2.putText(img,str(round(confidence* 100, 2)), (box[0] + 150, box[1] +30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 255, 0), 2)
 
 
-
-cv2.imshow('lena', img)
-cv2.waitKey(0)
+    cv2.imshow('lena', img)
+    cv2.waitKey(0)
